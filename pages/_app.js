04c14daa2ab3faab358f2242fb5f7a43c19faf0f
Jamie Barton <jamie@notrab.dev>
2020-11-23 20:51:55 +0000

    feat: add Stripe provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/tailwind.css";
 
 import { AnimatePresence } from "framer-motion";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 import { ThemeProvider } from "../context/theme";
 import { CartProvider } from "../context/cart";
@@ -8,18 +10,24 @@ import { CartProvider } from "../context/cart";
 import Cart from "../components/Cart";
 import Layout from "../components/Layout";
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
 function MyApp({ Component, pageProps, router }) {
   return (
-    <ThemeProvider>
-      <CartProvider>
-        <Cart />
-        <Layout>
-          <AnimatePresence initial={false} exitBeforeEnter>
-            <Component {...pageProps} key={router.route} />
-          </AnimatePresence>
-        </Layout>
-      </CartProvider>
-    </ThemeProvider>
+    <Elements stripe={stripePromise}>
+      <ThemeProvider>
+        <CartProvider>
+          <Cart />
+          <Layout>
+            <AnimatePresence initial={false} exitBeforeEnter>
+              <Component {...pageProps} key={router.route} />
+            </AnimatePresence>
+          </Layout>
+        </CartProvider>
+      </ThemeProvider>
+    </Elements>
   );
 }
 
