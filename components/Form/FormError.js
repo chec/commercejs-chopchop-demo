9420119f2b0cf1297475ef40d1a757eb9cc806ce
Jamie Barton <jamie@notrab.dev>
2020-12-07 18:26:05 +0000

    feat: use ErrorMessage component

--- components/Form/FormError.js
+++ components/Form/FormError.js
@@ -1,8 +1,18 @@
 import cc from "classcat";
+import { ErrorMessage } from "@hookform/error-message";
 
 function FormError({ className, ...props }) {
   return (
-    <span className={cc(["text-red-500 text-sm", className])} {...props} />
+    <div className="pt-1">
+      <ErrorMessage
+        {...props}
+        render={({ message }) => (
+          <span className={cc(["text-black text-sm", className])} {...props}>
+            {message}
+          </span>
+        )}
+      />
+    </div>
   );
 }
 
