9c0115aff7c341c3569c9e2b6c82b81d24f56f38
Jamie Barton <jamie@notrab.dev>
2020-12-09 22:46:42 +0000

    export FormError

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,3 +1,4 @@
 export { default as FormInput } from "./FormInput";
 export { default as FormCheckbox } from "./FormCheckbox";
 export { default as FormSelect } from "./FormSelect";
+export { default as FormError } from "./FormError";
