7e8772c82ee5876e3ba5c21e056965606ba51dfd
Jaeriah Tay <jaeriahpasseri@gmail.com>
2021-06-25 15:59:46 -0700

    move extrafields conditional inside of useEffect

--- components/Checkout/ExtraFieldsForm.js
+++ components/Checkout/ExtraFieldsForm.js
@@ -19,12 +19,12 @@ function ExtraFieldsForm() {
   const { extrafields } = useCheckoutState();
   const { setCurrentStep, nextStepFrom } = useCheckoutDispatch();
 
-  if (extrafields.length === 0) {
-    useEffect(() => {
+  useEffect(() => {
+    if (extrafields.length === 0) {
       setCurrentStep(nextStepFrom("extrafields"));
-    }, []);
+    }
     return null;
-  }
+  }, [extrafields]);
 
   return (
     <div className="md:flex md:space-x-12 lg:space-x-24">
